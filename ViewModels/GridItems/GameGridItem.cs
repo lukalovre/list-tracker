6d915ec6a9857b6801478364baa87e02d3bee9c6
Luka Lovre <lukalovre@protonmail.com>
2024-03-11 09:24:52 +0100

    removed unused props

--- ViewModels/GridItems/GameGridItem.cs
+++ ViewModels/GridItems/GameGridItem.cs
@@ -1,11 +1,4 @@
 using System;
 using AvaloniaApplication1.Models;
 
-public record GameGridItem(
-    int ID,
-    int Index,
-    string Title,
-    int Year,
-    string Platform,
-    int HLTB,
-    DateTime? Purchased) : IGridItem;
+public record GameGridItem(int ID, int Index, string Title, int Year) : IGridItem;
--- ViewModels/ItemViewModels/GamesViewModel.cs
+++ ViewModels/ItemViewModels/GamesViewModel.cs
@@ -6,15 +6,17 @@ namespace AvaloniaApplication1.ViewModels;
 public partial class GamesViewModel(IDatasource datasource, IExternal<Game> external)
 : ItemViewModel<Game, GameGridItem, GameItem>(datasource, external)
 {
+    // private SortableBindingList<Game1001> GetGamesFromCsv(string listName)
+    // {
+    //     return new SortableBindingList<Game1001>(
+    //         CsvHelper.GetFromList<IgdbListItem>(Paths.Igdb, listName, out _)
+    //         .Where(o => !Datasource.GetList<Game>().Any(g => g.Igdb == o.id) && !Igdb.AlternativeVersions.Keys.Contains(o.id))
+    //         .Select(o => Igdb.ConvertToGame1001(o))
+    //         .ToList());
+    // }
+
     public override GameGridItem Convert(int index, Game i)
     {
-        return new GameGridItem(
-            i.ID,
-            index + 1,
-            i.Title,
-            i.Year,
-            i.Platform,
-            i.HLTB,
-            i.Date);
+        return new GameGridItem(i.ID, index + 1, i.Title, i.Year);
     }
 }
