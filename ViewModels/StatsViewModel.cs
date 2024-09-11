8d45a51069a3247ef02df232bafa728af48ce78c
Luka Lovre <lukalovre@protonmail.com>
2024-09-11 15:33:08 +0200

    ref

--- ViewModels/StatsViewModel.cs
+++ ViewModels/StatsViewModel.cs
@@ -1,35 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using AvaloniaApplication1.ViewModels.Extensions;
 using Repositories;
 
 namespace AvaloniaApplication1.ViewModels;
 
-public partial class StatsViewModel(IDatasource datasource) : ItemViewModel<Game, GameGridItem, GameItem>(datasource)
+public partial class StatsViewModel(IDatasource datasource) : ViewModelBase
 {
-    public override GameGridItem Convert(int index, Game i)
-    {
-        var year = HtmlHelper.GetYear(i.release_date);
 
-        var developers = GetList(i.companies);
-        var platforms = GetList(i.platforms);
-
-        return new GameGridItem(
-            i.ID,
-            index + 1,
-            i.game,
-            year,
-            developers?.FirstOrDefault() ?? string.Empty,
-            platforms.FirstOrDefault() ?? string.Empty,
-            i.url);
-    }
-
-    private List<string> GetList(string companies)
-    {
-        companies = companies.TrimStart("[").TrimEnd("]");
-        var splitList = companies.Split(",");
-
-        return splitList.Select(o => o.TrimStart("\"").TrimEnd("\"")).ToList();
-    }
 }
