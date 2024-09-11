d6ea1797a4b5fef29403c33a9087504510c3bf7f
Luka Lovre <lukalovre@protonmail.com>
2024-09-11 15:31:37 +0200

    ref

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,11 +4,15 @@ namespace AvaloniaApplication1.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public MoviesViewModel MoviesViewModel { get; } = new MoviesViewModel(new CsvDatasource());
-    public GamesViewModel GamesViewModel { get; } = new GamesViewModel(new CsvDatasource());
-    public TVShowsViewModel TVShowsViewModel { get; } = new TVShowsViewModel(new CsvDatasource());
+    private static IDatasource _datasource;
 
-    public MainWindowViewModel()
+    public MoviesViewModel MoviesViewModel { get; } = new MoviesViewModel(_datasource);
+    public GamesViewModel GamesViewModel { get; } = new GamesViewModel(_datasource);
+    public TVShowsViewModel TVShowsViewModel { get; } = new TVShowsViewModel(_datasource);
+    public StatsViewModel StatsViewModel { get; } = new StatsViewModel(_datasource);
+
+    public MainWindowViewModel(IDatasource datasource)
     {
+        _datasource = datasource;
     }
 }
