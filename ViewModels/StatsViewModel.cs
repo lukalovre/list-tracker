2997b5e5aa631828fff32ff45e9d81b224ba9ef8
Luka Lovre <lukalovre@protonmail.com>
2024-09-11 15:43:56 +0200

    added LiveCharts2

--- ViewModels/StatsViewModel.cs
+++ ViewModels/StatsViewModel.cs
@@ -1,8 +1,13 @@
+using System.Collections.Generic;
 using Repositories;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
 
 namespace AvaloniaApplication1.ViewModels;
 
 public partial class StatsViewModel(IDatasource datasource) : ViewModelBase
 {
-
+    public List<Axis> _1001XAxes { get; set; } = [];
+    public List<ISeries> _1001 { get; } = [];
 }
